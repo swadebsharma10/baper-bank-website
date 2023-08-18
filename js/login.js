1a7b66d696c5ef63be708988023101ab2cd7c488
swadebsharma6 <swadebriva2012@gmail.com>
2023-08-18 13:06:28 +0600

    added bank site and user validation site

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // console.log('submit btn is clicked')
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+
+   if(email === 'swadeb2012@gmail.com' && password === '701012'){
+    // console.log('valid user')
+    window.location.href='bank.html'
+   }
+   else{
+    alert('Please provide Your valid Password')
+   }
+})
